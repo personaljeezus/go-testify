020244dc89287f8d3d251de5d75aa85792ef6959
Denis Kalugin <halfkotla@gmail.com>
2024-02-02 17:53:46 +0300

    last fix i hope

--- precode_test.go
+++ precode_test.go
@@ -12,30 +12,31 @@ import (
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=4&city=moscow", nil)
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
+	status := responseRecorder.Code
+	assert.Equal(t, http.StatusOK, status)
 	body := responseRecorder.Body.String()
 	list := strings.Split(body, ",")
-	if assert.Len(t, list, totalCount) {
-		assert.Equal(t, assert.Len, totalCount)
-	}
+	assert.Len(t, list, totalCount)
 }
 func TestIsYourReqCorrect(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=4&city=moscow", nil)
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
 	responseRecorder := httptest.NewRecorder()
 	require.NotEmpty(t, req)
 	status := responseRecorder.Code
-	assert.Equal(t, status, http.StatusOK)
+	assert.Equal(t, http.StatusOK, status)
 }
 func TestIfCityCorrect(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=1&city=moscow", nil)
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
-	actualCity := req.URL.Query().Get("city")
-	expected := "moscow"
-	assert.Equal(t, actualCity, expected)
-
+	status := responseRecorder.Code
+	assert.Equal(t, http.StatusOK, status)
+	city := req.URL.Query().Get("city")
+	list := strings.Split(city, ",")
+	assert.Len(t, list, 1)
 }
